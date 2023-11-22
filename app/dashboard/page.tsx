136affe52672b6487b59b33f5819be5b051cb464
Spookyless <spookyless.info@gmail.com>
2023-11-22 12:13:02 +0100

    Projects require signin, changed routing for landing page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,40 @@
 import {getSession} from "@utility/getSession";
 import {redirect} from "next/navigation";
-import SignOutButton from "@app/dashboard/SignOutButton";
+import Box from "@mui/material/Box";
+import {List, ListItem, ListItemButton, ListItemText, Typography} from "@mui/material";
+import {getClient} from "@app/ApolloClientRSC";
+import {GetProjectsDocument} from "@graphql/types";
 
 export default async function Component() {
-  const session = await getSession();
+    const session = await getSession();
 
-  if (!session || !session.user) {
-    return redirect('/signin');
-  }
+    if (!session || !session.user) {
+        return redirect('/signin');
+    }
 
-  return (
-    <div>
-      <div>Just for a second imagine this is the main dashboard</div>
-      <div>Your identity:</div>
-      <div>Name: {session.user.name}</div>
-      <div>Email: {session.user.email}</div>
-      <div>Image: {session.user.image}</div>
-      <div>Expires at: {session.expires}</div>
-      <br />
-      <div>To log out click the button below</div>
-      <SignOutButton />
-    </div>);
+    const {data} = await getClient().query({
+        query: GetProjectsDocument
+    });
+
+    return (
+        <Box>
+            <Typography variant="h1">Dashboard</Typography>
+
+            <Box sx={{height: "3rem"}} />
+
+            <Typography variant="h4">Your projects:</Typography>
+
+            <List>
+                {data.projects.map(p =>
+                    <ListItem key={p.uuid}>
+                        <ListItemButton href={`/project/${p.uuid}`}>
+                            <ListItemText>
+                                {p.name}
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                )}
+            </List>
+        </Box>
+    );
 }
