e9b1b5f08ba9af9581984cef889d7b233f43748e
Michal Karpierz <michalk101010@gmail.com>
2023-12-13 21:22:21 +0100

    Implemented drag and drop for column cards
    Known issues:
    - Clickable element cannot be a button and any interactive element
        - Source: https://github.com/atlassian/react-beautiful-dnd/issues/2296
    - Drop animation is disabled due to visual bug

--- components/Column/index.tsx
+++ components/Column/index.tsx
@@ -1,37 +1,64 @@
 import styles from './styles.module.scss'
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import {Button, Chip} from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import {ProjectQuery} from "@graphql/types";
-import ColumnCard from "@components/ColumnCard";
-import ColumnMenu from "@components/ColumnMenu";
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import { Button, Chip } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import { ProjectQuery } from '@graphql/types'
+import ColumnCard from '@components/ColumnCard'
+import ColumnMenu from '@components/ColumnMenu'
+import { Droppable } from 'react-beautiful-dnd'
 
-type Props = NonNullable<NonNullable<ProjectQuery['project']>["columns"]>[0]
+type Props = NonNullable<NonNullable<ProjectQuery['project']>['columns']>[0]
 
 function Column(props: Props) {
-    return (
-        <Paper className={styles.column} elevation={3} sx={{m: 2}}>
-            <Chip className={styles.column__tasksCounter} label={props.cards.length} size="small"/>
+  return (
+    <Paper className={styles.column} elevation={3} sx={{ m: 2 }}>
+      <Chip
+        className={styles.column__tasksCounter}
+        label={props.cards.length}
+        size="small"
+      />
 
-            <Typography className={styles.column__title} variant="h2">{props.name ?? "New column"}</Typography>
-            <Typography className={styles.column__type} variant="h3" fontSize={"small"}>{props.description ?? "Place for all your amazing ideas!"}</Typography>
+      <Typography className={styles.column__title} variant="h2">
+        {props.name ?? 'New column'}
+      </Typography>
+      <Typography
+        className={styles.column__type}
+        variant="h3"
+        fontSize={'small'}
+      >
+        {props.description ?? 'Place for all your amazing ideas!'}
+      </Typography>
 
-            <ColumnMenu
-                className={styles.column__menu}
-                columnType={props.type}
-                columnUuid={props.uuid}
-            />
+      <ColumnMenu
+        className={styles.column__menu}
+        columnType={props.type}
+        columnUuid={props.uuid}
+      />
 
-            <div>
-                {props.cards.map(card => (
-                    <ColumnCard key={card.uuid} {...card} />
-                ))}
-            </div>
+      <Droppable droppableId={props.uuid}>
+        {provided => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className={styles.column__cards}
+            style={{
+              minHeight: '20px',
+            }}
+          >
+            {props.cards.map((card, index) => (
+              <ColumnCard {...card} key={card.uuid} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
 
-            <Button className={styles.column__addTask}><AddIcon/> Add Card</Button>
-        </Paper>
-    )
+      <Button className={styles.column__addTask}>
+        <AddIcon /> Add Card
+      </Button>
+    </Paper>
+  )
 }
 
 export default Column
--- components/ColumnCard/index.tsx
+++ components/ColumnCard/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
-import { Button, Chip, Paper, Typography } from '@mui/material'
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import {ProjectQuery} from "@graphql/types";
-import {useDataContext} from "@app/DataContext";
+import { Chip, Paper, Typography } from '@mui/material'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import { ProjectQuery } from '@graphql/types'
+import { useDataContext } from '@app/DataContext'
+import {
+  Draggable,
+  DraggableStateSnapshot,
+  DraggingStyle,
+  NotDraggingStyle,
+} from 'react-beautiful-dnd'
 
-type Props = NonNullable<NonNullable<ProjectQuery['project']>["columns"]>[0]["cards"][0]
+function getStyle(
+  style: DraggingStyle | NotDraggingStyle | undefined,
+  snapshot: DraggableStateSnapshot
+) {
+  if (!snapshot.isDropAnimating) {
+    return style
+  }
+  return {
+    ...style,
+    // cannot be 0, but make it super tiny
+    transitionDuration: `0.001s`,
+  }
+}
+
+type Props = NonNullable<
+  NonNullable<ProjectQuery['project']>['columns']
+>[0]['cards'][0] & { index: number }
 
 function ColumnCard(props: Props) {
-  const { cardModal: { setCardUuid, setOpen } } = useDataContext()
+  const {
+    cardModal: { setCardUuid, setOpen },
+  } = useDataContext()
 
   const handleClick = () => {
     setCardUuid(props.uuid)
@@ -17,27 +41,49 @@ function ColumnCard(props: Props) {
   }
 
   return (
-    <Button className={styles.columncard} onClick={handleClick}>
-      <Paper className={styles.columncard__content} elevation={4}>
-        <Typography variant="body1" className={styles.columncard__name}>{props.name}</Typography>
-        {/*{labels && (*/}
-        {/*  <ul className={styles.columncard__labels}>*/}
-        {/*    {labels.map(label => (*/}
-        {/*      <li key={label.name}><Chip className={styles.columncard__label} label={label.name} sx={{ backgroundColor: label.color }} size="small" /></li>*/}
-        {/*    ))}*/}
-        {/*</ul>*/}
-        {/*)}*/}
-        {/*{assignee && (*/}
-        {/*  <Tooltip title={assignee}>*/}
-        {/*    <Avatar className={styles.columncard__assignee} src="/to/be/changed.png" />*/}
-        {/*  </Tooltip>*/}
-        {/*)}*/}
+    <Draggable
+      key={props.uuid}
+      draggableId={props.uuid}
+      index={props.index}
+      disableInteractiveElementBlocking={false}
+    >
+      {(provided, snapshot) => (
+        <div
+          className={styles.columncard}
+          onClick={handleClick}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={getStyle(provided.draggableProps.style, snapshot)}
+        >
+          <Paper className={styles.columncard__content} elevation={4}>
+            <Typography variant="body1" className={styles.columncard__name}>
+              {props.name}
+            </Typography>
+            {/*{labels && (*/}
+            {/*  <ul className={styles.columncard__labels}>*/}
+            {/*    {labels.map(label => (*/}
+            {/*      <li key={label.name}><Chip className={styles.columncard__label} label={label.name} sx={{ backgroundColor: label.color }} size="small" /></li>*/}
+            {/*    ))}*/}
+            {/*</ul>*/}
+            {/*)}*/}
+            {/*{assignee && (*/}
+            {/*  <Tooltip title={assignee}>*/}
+            {/*    <Avatar className={styles.columncard__assignee} src="/to/be/changed.png" />*/}
+            {/*  </Tooltip>*/}
+            {/*)}*/}
 
-        {props.deadline && (
-          <Chip className={styles.columncard__date} label={new Date(props.deadline).toLocaleDateString()} icon={<CalendarMonthIcon />} />
-        )}
-      </Paper>
-    </Button>
+            {props.deadline && (
+              <Chip
+                className={styles.columncard__date}
+                label={new Date(props.deadline).toLocaleDateString()}
+                icon={<CalendarMonthIcon />}
+              />
+            )}
+          </Paper>
+        </div>
+      )}
+    </Draggable>
   )
 }
 
